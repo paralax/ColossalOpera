9bf655a404a9673676b124ecb568b5a9ddda4822
jose nazario <jose.monkey.org@gmail.com>
2015-09-29 16:38:43 -0400

    gross, but gets the job done

---
+++ book/process.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import re
+import sys
+
+MINTEDOPS = r"""    [
+    framesep=2mm,
+    baselinestretch=1.2,
+    bgcolor=lightgray,
+    fontsize=\footnotesize,
+    linenos
+    ]
+"""
+
+MINTLANGMAPS={'F#': 'ocaml',
+              'Scala': 'javascript'}
+
+def main():
+    TITLE=False
+    DIFFICULTY=False
+    CODE=False
+    LANG=None
+    line=True
+    with sys.stdin as f:
+        while line:
+            line = f.readline() 
+            if line.startswith(r'\section{Title}'):
+                TITLE=True
+                continue
+            if TITLE and len(line.strip()) > 0:
+                print r'\section{%s}' % line.strip()
+                TITLE=False
+                continue
+            if line.startswith(r'\subsection{Difficulty}'):
+                DIFFICULTY=True
+                continue
+            if DIFFICULTY:
+                if line.startswith(r'\subsection{'):
+                    DIFFICULTY=False
+                    print line,
+                    continue
+                else:
+                    continue
+            if line.startswith(r'\subsection{'):
+                if 'Solution}' in line:
+                    CODE=True
+                    print line,
+                    LANG=re.findall(r"section.([A-Za-z]+) solution}", line, re.I)[0]
+                    continue
+            if CODE and line.startswith(r'\begin{verbatim}'):
+                print r'\begin{minted}'
+                print MINTEDOPS,
+                print '{%s}' % MINTLANGMAPS.get(LANG, LANG.lower())
+                LANG=None
+                continue
+            if CODE and line.startswith(r'\end{verbatim}'):
+                print r'\end{minted}'
+                CODE=False
+                continue
+            print line,
+
+if __name__ == '__main__':
+    main()
