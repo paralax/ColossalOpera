d90ac7951146968b63aea9c79c26c6d32bf978e6
Joe Reynolds <joereynolds952@gmail.com>
2016-04-01 19:57:46 +0100

    add install script

--- install.sh
+++ install.sh
@@ -1,5 +1,14 @@
-#required for tlmgr and latex
-sudo apt-get install texlive-full
+sudo apt-get install texlive-base
+sudo apt-get install texlive-latex-base
+sudo apt-get install texlive-latex-extra
+sudo apt-get install texlive-latex-recommended
+
+sudo apt-get install xzdec
 sudo apt-get install pandoc
 sudo easy_install Pygments
 sudo pip install Pygments
+
+tlmgr init-usertree
+tlmgr install
+tlmgr install mdframed
+tlmgr install xcolor
